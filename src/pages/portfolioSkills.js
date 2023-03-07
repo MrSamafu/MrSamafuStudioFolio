a42666ee03d2e19e3e515b731e31da375a7ea088
Samson <51507673+MrSamafu@users.noreply.github.com>
2023-03-07 13:49:30 +0100

    Add title to skill

--- src/pages/portfolioSkills.js
+++ src/pages/portfolioSkills.js
@@ -5,27 +5,34 @@ import styles from "../styles/Part.module.css";
 
 export default function PortfolioSkills() {
   return (
-    <Container className={styles.main}>
-      <Container className={styles.completePart}>
-        <Container className={styles.skill}>
-          <p className={styles.title}>Web</p>
-          <p className={styles.text}>
-            Création de site et d&apos;applications Web
-          </p>
+    <Container className={[styles.main, styles.columnPart].join(" ")}>
+      <Container className={styles.skills}>
+        <Container className={styles.titleSkillAlign}>
+          <p className={styles.title}>Compétences et services</p>
         </Container>
-        <Container className={styles.skill}>
-          <p className={styles.title}>BDD</p>
-          <p className={styles.text}>
-            Conception et création de base de donnée
-          </p>
-        </Container>
-        <Container className={styles.skill}>
-          <p className={styles.title}>Android</p>
-          <p className={styles.text}>Développement d&apos;application mobile</p>
-        </Container>
-        <Container className={styles.skill}>
-          <p className={styles.title}>Unity</p>
-          <p className={styles.text}>Conception de jeux vidéo</p>
+        <Container className={styles.cardAlign}>
+          <Container className={styles.skill}>
+            <p className={styles.title}>Web</p>
+            <p className={styles.text}>
+              Création de site et d&apos;applications Web
+            </p>
+          </Container>
+          <Container className={styles.skill}>
+            <p className={styles.title}>BDD</p>
+            <p className={styles.text}>
+              Conception et création de base de donnée
+            </p>
+          </Container>
+          <Container className={styles.skill}>
+            <p className={styles.title}>Android</p>
+            <p className={styles.text}>
+              Développement d&apos;application mobile
+            </p>
+          </Container>
+          <Container className={styles.skill}>
+            <p className={styles.title}>Unity</p>
+            <p className={styles.text}>Conception de jeux vidéo</p>
+          </Container>
         </Container>
       </Container>
     </Container>
